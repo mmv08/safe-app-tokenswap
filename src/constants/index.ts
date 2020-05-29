634a6338c9f4f57f284dc7d671e52def7005fc96
Mikhail Mikheev <mmvsha73@gmail.com>
2020-05-29 17:37:26 +0400

    api url wip

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+const getApiUrl = () => {
+  let url = "https://safe-transaction.staging.gnosisdev.com/api/v1/"
+
+  if (process.env.NODE_ENV === "production") {
+    if (process.env.NETWORK === "rinkeby") {
+      url = "https://safe-transaction.rinkeby.gnosis.io/api/v1/"
+    } else {
+      url = "https://safe-transaction.mainnet.gnosis.io/api/v1/"
+    }
+  }
+
+  return url
+}
+
+const API_URL = getApiUrl()
+
+export { API_URL }
